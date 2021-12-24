cf958c9ee64c6fae5c1cae3170ea15282d9ef9ae
Al-Dabbagh Harith <messi4ever500@gmail.com>
2021-12-24 13:29:07 +0300

    Added more components to be used later, improved the UI look, some cleanup

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import 'semantic-ui-css/semantic.min.css'; GIVES AN ERROR. DOUBLE SEMICOLON, check Q&A, search for "webpack"
+import 'react-calendar/dist/Calendar.css'
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
