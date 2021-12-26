2ae4ee8eec8ad315e773791a7df4968d6dd863df
Al-Dabbagh Harith <messi4ever500@gmail.com>
2021-12-26 11:58:54 +0300

    Implemented better forms for user input with validation using Formik, Yup and reactDatePicker

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 // import 'semantic-ui-css/semantic.min.css'; GIVES AN ERROR. DOUBLE SEMICOLON, check Q&A, search for "webpack"
 import 'react-calendar/dist/Calendar.css'
 import 'react-toastify/dist/ReactToastify.min.css'
+import 'react-datepicker/dist/react-datepicker.css'
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
